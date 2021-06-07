fd7ca9dc4c1ca4c92ed5c791fffa32fa8545cd4e
Abdysamat <shermende>
2021-06-07 06:58:46 +0000

    Initial commit

---
+++ src/test/java/dev/shermende/MicronautWebTest.java
@@ -0,0 +1,21 @@
+package dev.shermende;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class MicronautWebTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
